eb05b40bf44906190247071c334dae4e78590041
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-01-14 15:15:50 -0500

    added auth design recipe

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,10 @@
 const jwt = require("jsonwebtoken");
 
+// authenticate : Request Response function(Object) -> void
+// async function that will extract and verify JWT is valid and not blacklisted
+// and calls callback with the decoded JWT object passed as only argument
+// or responds with appropriate error if not
+
 function authenticate(req, res, callback) {
   const fullToken = req.headers.authorization || "";
   const token = fullToken.split(" ")[1];
@@ -12,5 +17,28 @@ function authenticate(req, res, callback) {
     callback(decoded);
   });
 }
+
+/*
+Example:
+
+...
+
+  authenticate(req, res, (decoded) => {
+    //if this callback is called that means everything is ok
+
+    //you can extract values from decoded like so:
+
+    const jti = decoded.jti;
+    const iat = decoded.iat;
+
+    //and use req and res as you would normally do
+
+    res.status(200).json("Congrats! You are authorized to view this!")
+
+  });
+
+...
+
+*/
 
 module.exports = authenticate;
